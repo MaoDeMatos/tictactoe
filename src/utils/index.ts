8e922331f6a85d269f5a197feb427bf4d641c9ef
Mao De Matos <mao.dematos@gmail.com>
2022-07-05 22:30:57 +0200

    💬 Add const to store `messages` to display

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,21 @@
 export function capitalizeFirstLetter(text: string) {
   return text.charAt(0).toUpperCase() + text.slice(1);
 }
+
+export const messagesToDisplay = {
+  // Informations
+  enterYourName: {
+    error: false,
+    message: "Enter your name !",
+  },
+  initBoard: {
+    error: false,
+    message: "Click on an empty box to place a circle and start playing !",
+  },
+  playerTurn: (playerName: string) => `It's ${playerName}'s turn`,
+  // Errors
+  nameError: {
+    error: true,
+    message: "You must enter a valid name !",
+  },
+};
