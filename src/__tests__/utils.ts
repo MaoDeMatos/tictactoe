adc1295b44f59a2b60bcdf902bbb2d2b40e2dc65
Mao De Matos <mao.dematos@gmail.com>
2022-07-08 23:10:35 +0200

    🧪 ♻️ Refactor test utility

--- src/__tests__/utils.ts
+++ src/__tests__/utils.ts
@@ -1,20 +1,14 @@
-import { BoardType, CellType } from "../types/GeneralTypes";
+import { CellType } from "../types/GeneralTypes";
 
 import { winningCombinations } from "../utils";
 import { ParametrizedTestBoards } from "./types";
 
-export function generateWinningBoards(symbol: CellType) {
-  const newBoards: ParametrizedTestBoards = [];
-
-  winningCombinations.forEach((combination, index) => {
-    const array: BoardType = new Array(9).fill(null);
-
-    combination.forEach(idx => {
-      array[idx - 1] = symbol;
-    });
-
-    newBoards.push({ board: array, desc: `combination ${index + 1}` });
-  });
-
-  return newBoards;
-}
+export const generateWinningBoards = (
+  symbol: CellType
+): ParametrizedTestBoards =>
+  winningCombinations.map((combination, index) => ({
+    board: new Array(9)
+      .fill(null)
+      .map((_, idx) => (combination.includes(idx + 1) ? symbol : null)),
+    desc: `combination ${index + 1}`,
+  }));
